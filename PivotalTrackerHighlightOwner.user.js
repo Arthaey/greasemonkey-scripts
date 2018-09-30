13e9419439af104138e9090136db9de025d9dcec
Arthaey Angosii <arthaey@gmail.com>
2018-09-30 08:28:13 -0700

    Update for slow loading of async data.

--- PivotalTrackerHighlightOwner.user.js
+++ PivotalTrackerHighlightOwner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Highlight Pivotal Tracker Story Owner
 // @namespace    http://arthaey.com/
-// @version      1.0
+// @version      1.1
 // @description  Highlight your own stories
 // @author       Arthaey Angosii <arthaey@gmail.com>
 // @match        https://www.pivotaltracker.com/n/projects/*
@@ -9,13 +9,30 @@
 // ==/UserScript==
 
 (function() {
-  const owner = document.querySelector(".AvatarDetails__name").innerText;
+  let intervalId;
+  const initialHighlightDelay = 1000;
+  const checkForUpdatesDelay = 10000;
 
-  function highlight() {
-    document.querySelectorAll(`[title="${owner}"]`).forEach(elem => {
-      elem.style.backgroundColor = "#FFEB3B";
-    });
+  function run() {
+    const profileButton = document.querySelector(".Dropdown button");
+    if (profileButton) {
+      profileButton.click();
+      const owner = document.querySelector(".AvatarDetails__name").innerText;
+      profileButton.click();
+
+      const highlightOwner = function() { highlight(owner) };
+      clearInterval(intervalId);
+      setTimeout(highlightOwner, initialHighlightDelay);
+      setInterval(highlightOwner, checkForUpdatesDelay);
+    }
   }
 
-  setInterval(highlight, 1000); // in case of new or modified stories
+  function highlight(owner) {
+    if (!owner) return;
+    const stories = document.querySelectorAll(`[title="${owner}"]`);
+    stories.forEach(elem => elem.style.backgroundColor = "#FFEB3B");
+  }
+
+  intervalId = setInterval(run, 500); // takes a moment to load
+
 })();
